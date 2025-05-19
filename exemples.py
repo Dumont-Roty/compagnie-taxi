edd10fb05add1ed73564d22ed59211857785327b
Pierre DUMONT ROTY <pierre-dr@hotmail.com>
2025-05-19 20:49:25 +0200

    EstVoisin Exemple fonctionnel.
    J'ai fait des exemples de code pour illustrer comment utiliser la fonction EstVoisin.
    // La fonction EstVoisin vérifie si deux points sont voisins dans une grille 2D.
    Je dois faire des raiseValue Error si les coordonnées sont en dehors de la grille.

--- exemples.py
+++ exemples.py
@@ -1,28 +1,45 @@
 from src.compagnie_taxi.reseau import *
 
-# Ecriture des voisins sous format de liste.
+def EstVoisin(a, b):
+    """
+    Fonction qui permet de savoir si deux emplacements sont voisins
+    :param a: numéro du premier emplacement
+    :param b: numéro du deuxième emplacement
+    :return: True si les emplacements sont voisins, False sinon
+    """
+    emplacements = [None] + [emplacement(i, False) for i in range(1, 17)]
+    voisinages = [
+        (1, 2, 5), (1, 3, 9), (1, 4, 4),
+        (2, 5, 3), (2, 6, 2),
+        (3, 4, 4), (3, 6, 1),
+        (4, 7, 7),
+        (5, 8, 4), (5, 9, 2), (5, 10, 9),
+        (6, 7, 3), (6, 10, 9), (6, 11, 6),
+        (7, 11, 8), (7, 15, 5),
+        (8, 12, 5),
+        (9, 8, 3), (9, 13, 10),
+        (10, 9, 6), (10, 13, 5), (10, 14, 1),
+        (11, 14, 2),
+        (12, 16, 9),
+        (13, 12, 4), (13, 14, 3),
+        (14, 16, 4),
+        (15, 14, 4), (15, 16, 3)
+    ]
 
-emplacements = {i: emplacement(i, False) for i in range(1, 17)}
+    for x, y, duree in voisinages:
+        emplacements[x].voisins.append((emplacements[y], duree))
+        emplacements[y].voisins.append((emplacements[x], duree))
 
-voisinages = [
-    (1, 2, 5), (1, 3, 9), (1, 4, 4),
-    (2, 5, 3), (2, 6, 2),
-    (3, 4, 4), (3, 6, 1),
-    (4, 7, 7),
-    (5, 8, 4), (5, 9, 2), (5, 10, 9),
-    (6, 7, 3), (6, 10, 9), (6, 11, 6),
-    (7, 11, 8), (7, 15, 5),
-    (8, 12, 5),
-    (9, 8, 3), (9, 13, 10),
-    (10, 9, 6), (10, 13, 5), (10, 14, 1),
-    (11, 14, 2),
-    (12, 16, 9),
-    (13, 12, 4), (13, 14, 3),
-    (14, 16, 4),
-    (15, 14, 4), (15, 16, 3)
-]
+    # Vérifie si emplacements[a] est voisin de emplacements[b]
+    est_voisin = any(voisin.numero == a for voisin, _ in emplacements[b].voisins)
+    if est_voisin:
+        print(f"{a} et {b} sont voisins")
+        return True
+    else:
+        print(f"{a} et {b} ne sont pas voisins")
+        return False
 
-for a, b, duree in voisinages:
-    emplacements[a].voisins.append((emplacements[b], duree))
-    emplacements[b].voisins.append((emplacements[a], duree))
-    
+EstVoisin(1, 2)
+EstVoisin(10, 1)
+EstVoisin(8, 12)
+
